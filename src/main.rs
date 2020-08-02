24ecd8e4d2a0c61474ffc2f97e62ea649d505ecc
Francisco Knebel <franciscopaivaknebel@gmail.com>
2020-08-02 18:17:29 -0300

    Notification counter and ENV defined topic.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use tokio::{task, time};
 // use std::thread;
 
 use std::env;
-use rumqttc::{MqttOptions, QoS, EventLoop, Request, Publish};
+use rumqttc::{MqttOptions, QoS, EventLoop, Request, Publish, Incoming, Outgoing};
 use std::time::Duration;
 use std::fs::File;
 use std::io::{BufReader};
@@ -26,6 +26,15 @@ extern crate lazy_static;
 
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
+
+#[derive(Debug)]
+struct NotificationCounter {
+  Pub: u64,
+  PubAck: u64,
+  PubRec: u64,
+  PubRel: u64,
+  PubComp: u64
+}
 
 fn handle_close(published: Arc<Mutex<HashMap<String, i64>>>) {
   info!("Closing client application.");
@@ -58,57 +67,60 @@ async fn main() {
   let buffer_size = env::var("MQTT_CLIENT_BUFFER_BYTE_SIZE").unwrap_or("8".to_string()).parse::<usize>().unwrap();
   let message_limit = env::var("MQTT_CLIENT_MESSAGES_TO_SEND").unwrap_or("100".to_string()).parse::<u64>().unwrap();
   let message_delay_ms = env::var("MQTT_CLIENT_MESSAGE_DELAY_MS").unwrap_or("0".to_string()).parse::<u64>().unwrap();
+  let topic = env::var("MQTT_CLIENT_TOPIC").unwrap();
 
   info!("Starting client. Host at {}:{}", address.clone(), port.clone());
   
-  let count = get_lines();
   let mut mqttoptions = MqttOptions::new("client", address.clone(), port);
   mqttoptions.set_keep_alive(30);
 
   let mut eventloop = EventLoop::new(mqttoptions, 20).await;
   let requests_tx = eventloop.handle();
   
-  let tx_c = requests_tx.clone();
-  for i in 0..count {
-    let topic = get_topic(i);
+  let tx = requests_tx.clone();
+  let published_cl = Arc::clone(&published);
     
-    let tx = tx_c.clone();
-    let published_cl = Arc::clone(&published);
+  task::spawn(async move {
+    info!("Thread of topic {}", topic.clone());
+    let p = Arc::clone(&published_cl);
     
-    task::spawn(async move {
-      info!("Thread {} - {}", i, topic.clone());
-      let p = Arc::clone(&published_cl);
-      
-      let mut index: u64 = 0;
-      while index < message_limit {
-        let index_str = index.to_string();
-        let mut payload: String = std::iter::repeat(())
-          .map(|()| thread_rng().sample(Alphanumeric))
-          .take(buffer_size - index_str.len() - 1).collect();
+    let mut index: u64 = 0;
+    while index < message_limit {
+      let index_str = index.to_string();
+      let mut payload: String = std::iter::repeat(())
+        .map(|()| thread_rng().sample(Alphanumeric))
+        .take(buffer_size - index_str.len() - 1).collect();
 
-        payload.insert_str(0, &" ");
-        payload.insert_str(0, &index_str);
+      payload.insert_str(0, &" ");
+      payload.insert_str(0, &index_str);
 
-        let t = topic.clone();
+      let t = topic.clone();
 
-        tx.send(publish_request(&(payload.as_str()), &t.clone())).await.unwrap();
-        
-        {
-          // Access global mutexed variable
-          let mut guard = p.lock().unwrap();
-          guard.entry(t.clone()).or_insert(0);
-          guard.insert(t.clone(), 1);
-          drop(guard);
-        }
+      tx.send(publish_request(&(payload.as_str()), &t.clone())).await.unwrap();
 
-        info!("{}::{} \"{}\"", index, t, payload);
-
-        index += 1;
-        time::delay_for(Duration::from_millis(message_delay_ms)).await;
+      {
+        // Access global mutexed variable
+        let mut guard = p.lock().unwrap();
+        guard.entry(t.clone()).or_insert(0);
+        guard.insert(t.clone(), 1);
+        drop(guard);
       }
-      info!("Thread {} done with {} messages.", i, index);
-    });
-  }
+
+      info!("{}::{} \"{}\"", index, t, payload);
+
+      index += 1;
+      time::delay_for(Duration::from_millis(message_delay_ms)).await;
+    }
+    info!("Thread {} done with {} messages.", topic, index);
+  });
+
+  let mut notification_counter = NotificationCounter {
+    Pub: 0,
+    PubAck: 0,
+    PubRec: 0,
+    PubRel: 0,
+    PubComp: 0
+  };
 
   loop {
     match eventloop.poll().await {
@@ -116,15 +128,45 @@ async fn main() {
         let (incoming, outgoing) = _notification;
 
         if incoming.is_some() {
-          info!("INC: {:?}", incoming);
+          match incoming.unwrap() {
+            Incoming::PubAck(ack) => {
+              info!("{:?}", ack);
+              notification_counter.PubAck += 1;
+            },
+            Incoming::PubRec(ack) => {
+              info!("{:?}", ack);
+              notification_counter.PubRec += 1;
+            },
+            Incoming::PubRel(ack) => {
+              info!("{:?}", ack);
+              notification_counter.PubRel += 1;
+            },
+            Incoming::PubComp(ack) => {
+              info!("{:?}", ack);
+              notification_counter.PubComp += 1;
+            },
+            _ => {}
+          }
         }
 
         if outgoing.is_some() {
-          info!("OUT: {:?}", outgoing);
+          match outgoing.unwrap() {
+            Outgoing::Publish(publish) => {
+              info!("{:?}", publish);
+              notification_counter.Pub += 1;
+            },
+            _ => {}
+          }
+
         }
       },
       Err(e) => { error!("{:?}", e); }
     }
+
+    if notification_counter.Pub == message_limit {
+      info!("{:#?}", notification_counter);
+    }
+
     time::delay_for(Duration::from_millis(0)).await;
   }
 }
@@ -148,28 +190,4 @@ fn publish_request(payload: &str, topic: &str) -> Request {
 
   let publish = Publish::new(&topic, qos, message);
   Request::Publish(publish)
-}
-
-fn get_topic(line_num: usize) -> String {
-  let f = File::open("topic_names.txt").expect("Open topic file");
-  let buffer = BufReader::new(f);
-  let mut topic = String::new();
-
-  let mut counter = 0;
-
-  for line in buffer.lines() {
-    if counter == line_num {
-      topic = line.unwrap();
-    }
-    counter += 1;
-  }
-
-  topic
-}
-
-fn get_lines() -> usize {
-  let f = File::open("topic_names.txt").expect("Open topic file");
-  let buffer = BufReader::new(f);
-
-  buffer.lines().count()
 }
